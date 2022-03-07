3dc997bfadf12c7d7a65e97ff221d103bc2aaeae
Quincy Prioleau <Quincetrace777@mac.com>
2022-03-07 05:57:57 -0500

    Initial Commit

---
+++ LearningGitH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningGitH
+//
+//  Created by Quincy Prioleau on 3/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearningGitH/LearningGitHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGitHApp.swift
+//  LearningGitH
+//
+//  Created by Quincy Prioleau on 3/7/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGitHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
